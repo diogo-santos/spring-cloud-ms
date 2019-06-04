a99c5994985b15fa37e06aaac64153bdcfb1fbab
diogo-santos <diogoffs@outlook.com>
2019-06-04 22:41:20 +0100

    Addd microservice module

---
+++ person-services/src/main/java/com/example/services/PersonServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonServicesApplication.class, args);
+	}
+
+}
