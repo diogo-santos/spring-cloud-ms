2a20e33a45202d6f725d9018724df864a4fba011
diogo-santos <diogoffs@outlook.com>
2019-06-09 19:06:47 +0100

    Enable discovery client

--- person-services/src/main/java/com/example/services/PersonServicesApplication.java
+++ person-services/src/main/java/com/example/services/PersonServicesApplication.java
@@ -2,6 +2,7 @@ package com.example.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -14,15 +15,17 @@ import static springfox.documentation.builders.PathSelectors.any;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableDiscoveryClient
 public class PersonServicesApplication {
-
 	@Bean
 	public Docket api(){
-		return new Docket(DocumentationType.SWAGGER_2).groupName("Person").select()
+		return new Docket(DocumentationType.SWAGGER_2)
+				.groupName("Person")
+				.select()
 				.apis(RequestHandlerSelectors.basePackage("com.example.services"))
-				.paths(any()).build().apiInfo(new ApiInfo("Person Services",
-						"A set of services to provide data access to person", "0.0.1", null,
-						new Contact("Diogo S.", "http://github.com/diogo-santos", null),null, null));
+				.paths(any())
+				.build()
+				.apiInfo(new ApiInfo("Person Services","A set of services to provide data access to person", "0.0.1", null,new Contact("Diogo S.", "http://github.com/diogo-santos", null),null, null));
 	}
 
 	public static void main(String[] args) {
