07f7429b00875efb51f5310f0781c52b3ad62991
diogo-santos <diogoffs@outlook.com>
2019-08-10 19:15:51 +0100

    Enhancing tests person-webapp

--- person-webapp/src/test/java/com/example/web/PersonWebappApplicationTests.java
+++ person-webapp/src/test/java/com/example/web/PersonWebappApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class PersonWebappApplicationTests {
 	@Autowired
 	private MockMvc mockMvc;
