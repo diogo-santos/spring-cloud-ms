e09c6308e200beb81c676e370508542cfeb5763c
diogo-santos <diogoffs@outlook.com>
2019-08-11 23:59:34 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-
-echo "Building projects..."
-mvn clean package
-
 echo "Starting config server ..."
 gnome-terminal -- java -jar config-server/target/config-server-0.0.1-SNAPSHOT.jar
 
