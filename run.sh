6cac479541ea11b7379edd607b50ad59b42eb334
diogo-santos <diogoffs@outlook.com>
2019-08-20 23:33:08 +0100

    Updating run.sh

--- run.sh
+++ run.sh
@@ -7,16 +7,17 @@ echo "Starting eureka server ..."
 gnome-terminal -- java -jar eureka-server/target/eureka-server-0.0.1-SNAPSHOT.jar
 
 while ! nc -z localhost 8888 ; do
-    echo "Waiting for upcoming Config Server"
+    echo "Waiting for upcoming config server..."
     sleep 10
 done
 
 echo "Starting services..."
+gnome-terminal -- java -jar gateway-server/target/gateway-server-0.0.1-SNAPSHOT.jar
 gnome-terminal -- java -jar person-services/target/person-services-0.0.1-SNAPSHOT.jar
 gnome-terminal -- java -jar person-contact-services/target/person-contact-services-0.0.1-SNAPSHOT.jar
 gnome-terminal -- java -jar person-business-services/target/person-business-services-0.0.1-SNAPSHOT.jar
 gnome-terminal -- java -jar person-webapp/target/person-webapp-0.0.1-SNAPSHOT.jar
 
-echo "Check Eureka server at localhost:8761 in order to see all registered services"
+echo "All services have started"
 
 exit 0
