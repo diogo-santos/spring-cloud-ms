6e1b00b50162762496445f3a107c8763319cf985
diogo-santos <diogoffs@outlook.com>
2019-08-20 23:35:17 +0100

    Replacing Open Feign by Reactive WebClient api for client rest calls

--- person-webapp/src/main/java/com/example/cloud/web/client/PersonService.java
+++ person-webapp/src/main/java/com/example/cloud/web/client/PersonService.java
@@ -1,22 +1,9 @@
 package com.example.cloud.web.client;
 
 import com.example.cloud.web.domain.Person;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.List;
-
-@FeignClient(value="GATEWAY-SERVER/person-service", fallback = PersonServiceFallbackImpl.class)
 public interface PersonService {
-    @RequestMapping(value="/person", method= RequestMethod.GET)
-    List<Person> findAll();
-
-    @RequestMapping(value="/person/{id}", method = RequestMethod.GET)
-    Person findById(@PathVariable("id") long id);
-
-    @RequestMapping(value="/person", method = RequestMethod.POST)
-    Person create(@RequestBody Person person);
+    Person[] findAll();
+    Person findById(long id);
+    Person create(Person person);
 }
